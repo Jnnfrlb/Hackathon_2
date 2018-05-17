cc8449083a1fa19abddfc8133acb06e5273bc095
ccoralie <coraliecotte.cc@gmail.com>
2018-05-17 16:48:12 +0200

    ajout login/register

---
+++ hackaton2/src/AppBundle/Entity/User.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="`user`")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+
+    /*
+     * Adding Personal methods/variables
+     */
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+    public function __toString()
+    {
+        // Return the User object with "[IACO] - [NAME] [CITY]" format, when  __toString is called.
+        return $this->firstName;
+    }
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstName", type="string", length=32)
+     */
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstName", type="string", length=32)
+     */
+    private $firstName;
+
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+}

455c092690c7213fd64d315b5572f0dbdab9b58e
zakaria zekraoui <zakaria.zekraoui@gmail.com>
2018-05-17 17:04:02 +0200

    creation page accueil

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,11 @@ Encore
     .addEntry('app', './assets/js/app.js')
     .addEntry('jquery', './assets/js/style.js')
     .addEntry('style', './assets/scss/main.scss')
+
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
     .enableSassLoader(function(sassOptions) {}, {
               resolveUrlLoader: false});
+
 
 module.exports = Encore.getWebpackConfig();

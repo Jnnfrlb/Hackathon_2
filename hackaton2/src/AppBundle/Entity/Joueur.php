8a59e39166b61ef7d5047dd9b31d7f120b17e0e9
ccoralie <coraliecotte.cc@gmail.com>
2018-05-17 11:20:45 +0200

    ajout CRUD joueur/equipe/match

---
+++ hackaton2/src/AppBundle/Entity/Joueur.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Joueur
+ *
+ * @ORM\Table(name="joueur")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\JoueurRepository")
+ */
+class Joueur
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="arme", type="string", length=255)
+     */
+    private $arme;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="age", type="smallint")
+     */
+    private $age;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="photo", type="text")
+     */
+    private $photo;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Joueur
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set arme
+     *
+     * @param string $arme
+     *
+     * @return Joueur
+     */
+    public function setArme($arme)
+    {
+        $this->arme = $arme;
+
+        return $this;
+    }
+
+    /**
+     * Get arme
+     *
+     * @return string
+     */
+    public function getArme()
+    {
+        return $this->arme;
+    }
+
+    /**
+     * Set age
+     *
+     * @param integer $age
+     *
+     * @return Joueur
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    /**
+     * Get age
+     *
+     * @return int
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * Set photo
+     *
+     * @param string $photo
+     *
+     * @return Joueur
+     */
+    public function setPhoto($photo)
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    /**
+     * Get photo
+     *
+     * @return string
+     */
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+}
+
